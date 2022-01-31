2f88e29e87b3af81c8d5b834daee3938a9caeea4
liefwerk <natjageni@gmail.com>
2022-01-31 20:55:36 -0300

    added OSS projects

---
+++ src/_data/oss_projects.js
@@ -0,0 +1,26 @@
+module.exports = [
+	{ 
+		title: 'TNB Analytics',
+		card_description: 'NuxtJS static page that serves cryptomoney (TNBC) Analytics from a Django Backend.',
+		tags: [
+			{ name: 'NuxtJS', color: 'bg-yellow-800' },
+			{ name: 'Static Website', color: 'bg-red-900' },
+			{ name: 'Github Pages', color: 'bg-gray-600' }
+		],
+		link: 'http://tnbanalytics.com/',
+		github_link: 'https://github.com/Techminate/TNBC-POS-Frontend',
+		image: 'tnb_analytics'
+	},
+	{ 
+		title: 'TNB Point Of Sale',
+		card_description: 'Vue3 web app serving as a point of sale terminal for commerces (with a Laravel backend).',
+		tags: [
+			{ name: 'Vue3', color: 'bg-yellow-800' },
+			{ name: 'Web App', color: 'bg-red-900' },
+			{ name: 'Netlify', color: 'bg-gray-600' }
+		],
+		link: 'https://tnb-pos.netlify.com',
+		github_link: 'https://github.com/Techminate/TNBC-POS-Frontend',
+		image: 'tnbc_pos'
+	},
+]
